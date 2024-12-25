f0b1c1b05fdff4bd36754340bcb7142e4e342924
Adam Byrne <adam.r.byrne@gmail.com>
2024-12-25 17:59:29 +0000

    refactor: remove unused logger output function and clean up tests

--- internal/logger/logger_test.go
+++ internal/logger/logger_test.go
@@ -1,13 +1,12 @@
 package logger_test
 
 import (
-	"bytes"
-	"strings"
 	"testing"
 
 	"github.com/bxrne/launchrail/internal/logger"
 )
 
+// TEST: GIVEN GetLogger is called THEN a non-nil logger is returned
 func TestGetLogger(t *testing.T) {
 	log := logger.GetLogger()
 	if log == nil {
@@ -15,20 +14,12 @@ func TestGetLogger(t *testing.T) {
 	}
 }
 
+// TEST: GIVEN GetLogger is called multiple times THEN the logger is a singleton
 func TestGetLoggerSingleton(t *testing.T) {
 	log1 := logger.GetLogger()
 	log2 := logger.GetLogger()
 
 	if log1 != log2 {
 		t.Error("Expected logger to be a singleton")
-	}
-}
-
-func TestLoggerOutput(t *testing.T) {
-	var buf bytes.Buffer
-	logger.SetLoggerOutput(&buf)
-	logger.GetLogger().Info("test message")
-	if !strings.Contains(buf.String(), "test message") {
-		t.Errorf("Expected logger output to contain 'test message', got: %s", buf.String())
 	}
 }
