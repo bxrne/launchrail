d96fab6780600d63d81f507bb836d72f5cc3ee09
Adam Byrne <adam.r.byrne@gmail.com>
2025-01-26 21:03:53 +0000

    reduce cognitive complexity of world update for physics

--- pkg/ecs/systems/physics_system.go
+++ pkg/ecs/systems/physics_system.go
@@ -23,36 +23,75 @@ func (s *PhysicsSystem) Priority() int {
 }
 
 func (s *PhysicsSystem) Update(world *ecs.World, dt float64) error {
-	var err error
 	entities := world.Query(ecs.ComponentPhysics)
 
-	// Process physics in parallel
+	// Create a channel to collect errors from goroutines
+	errChan := make(chan error, s.workers)
+
+	// Distribute work across workers
 	chunkSize := (len(entities) + s.workers - 1) / s.workers
 	s.wg.Add(s.workers)
 
 	for w := 0; w < s.workers; w++ {
-		start := w * chunkSize
-		end := start + chunkSize
-		if end > len(entities) {
-			end = len(entities)
-		}
-
-		go func(start, end int) {
-			defer s.wg.Done()
-			for i := start; i < end; i++ {
-				entity := entities[i]
-				if comp, ok := world.GetComponent(entity, ecs.ComponentPhysics); ok {
-					if physics, ok := comp.(ecs.PhysicsComponent); ok {
-						err = physics.Update(dt)
-						if err != nil {
-							return
-						}
-					}
-				}
-			}
-		}(start, end)
+		go s.processEntityChunk(world, entities, w, chunkSize, dt, errChan)
 	}
 
+	// Wait for all workers to complete
 	s.wg.Wait()
-	return err
+	close(errChan)
+
+	// Collect first error if any
+	return s.collectFirstError(errChan)
+}
+
+// Process a chunk of entities for a specific worker
+func (s *PhysicsSystem) processEntityChunk(
+	world *ecs.World,
+	entities []ecs.EntityID,
+	workerIndex,
+	chunkSize int,
+	dt float64,
+	errChan chan<- error,
+) {
+	defer s.wg.Done()
+
+	start := workerIndex * chunkSize
+	end := start + chunkSize
+	if end > len(entities) {
+		end = len(entities)
+	}
+
+	for i := start; i < end; i++ {
+		if err := s.updateEntityPhysics(world, entities[i], dt); err != nil {
+			errChan <- err
+			return
+		}
+	}
+}
+
+// Update physics for a single entity
+func (s *PhysicsSystem) updateEntityPhysics(
+	world *ecs.World,
+	entity ecs.EntityID,
+	dt float64,
+) error {
+	comp, ok := world.GetComponent(entity, ecs.ComponentPhysics)
+	if !ok {
+		return nil
+	}
+
+	physics, ok := comp.(ecs.PhysicsComponent)
+	if !ok {
+		return nil
+	}
+
+	return physics.Update(dt)
+}
+
+// Collect the first error from error channel
+func (s *PhysicsSystem) collectFirstError(errChan <-chan error) error {
+	for err := range errChan {
+		return err
+	}
+	return nil
 }
