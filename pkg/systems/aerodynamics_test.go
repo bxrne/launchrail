32daf870776716c3180991546ce14e0099da3caa
Adam Byrne <adam.r.byrne@gmail.com>
2025-02-03 14:17:15 +0000

    feat: Add unit tests for AerodynamicSystem functionality and create launch rail test file

--- pkg/systems/aerodynamics_test.go
+++ pkg/systems/aerodynamics_test.go
@@ -43,3 +43,65 @@ func TestAerodynamicSystem_CalculateDrag(t *testing.T) {
 	drag := aero.CalculateDrag(entity)
 	require.NotNil(t, drag)
 }
+
+// TEST: GIVEN an AerodynamicSystem WHEN Update is called THEN the system state is updated
+func TestAerodynamicSystem_Update(t *testing.T) {
+	world := &ecs.World{}
+	workers := 1
+	cfg := &config.Config{}
+	aero := systems.NewAerodynamicSystem(world, workers, cfg)
+	require.NotNil(t, aero)
+
+	err := aero.Update(0.1)
+	require.NoError(t, err)
+}
+
+// TEST: GIVEN a new AerodynamicsSystem WHEN Add is called THEN the entity is added to the system
+func TestAerodynamicSystem_Add(t *testing.T) {
+	world := &ecs.World{}
+	workers := 1
+	cfg := &config.Config{}
+	aero := systems.NewAerodynamicSystem(world, workers, cfg)
+	require.NotNil(t, aero)
+
+	// Create a physics entity
+	entity := systems.PhysicsEntity{
+		Entity:       &ecs.BasicEntity{},
+		Position:     &components.Position{Y: 0},
+		Velocity:     &components.Velocity{X: 0},
+		Acceleration: &components.Acceleration{},
+		Mass:         &components.Mass{},
+		Motor:        &components.Motor{},
+		Bodytube:     &components.Bodytube{},
+		Nosecone:     &components.Nosecone{},
+	}
+
+	aero.Add(&entity)
+}
+
+// TEST: GIVEN a new AerodynamicsSystem WHEN Priority is called THEN the system priority is returned
+func TestAerodynamicSystem_Priority(t *testing.T) {
+	world := &ecs.World{}
+	workers := 1
+	cfg := &config.Config{}
+	aero := systems.NewAerodynamicSystem(world, workers, cfg)
+	require.NotNil(t, aero)
+
+	priority := aero.Priority()
+	require.Equal(t, 2, priority)
+}
+
+// TEST: GIVEN a new AerodynamicsSystem WHEN GetSpeedOfSound is called THEN the speed of sound is returned
+func TestAerodynamicSystem_GetSpeedOfSound(t *testing.T) {
+	world := &ecs.World{}
+	workers := 1
+	cfg := &config.Config{}
+	aero := systems.NewAerodynamicSystem(world, workers, cfg)
+	require.NotNil(t, aero)
+
+	speed := aero.GetSpeedOfSound(20)
+	require.Greater(t, speed, 0.0)
+}
+
+// TEST: GIVEN a new AerodynamicsSystem
+// TEST: GIVEN a new AerodynamicsSystem
