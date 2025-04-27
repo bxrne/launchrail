c4c3f8092ae3f88aab376673d5fe19d5d0a54266
Adam Byrne <adam.r.byrne@gmail.com>
2025-04-27 19:03:26 +0100

    test(plugin_manager): Fixed success testcase

--- internal/plugin/manager_test.go
+++ internal/plugin/manager_test.go
@@ -1,16 +1,16 @@
 package plugin_test
 
 import (
-	"testing"
-
-	"github.com/bxrne/launchrail/internal/plugin"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"testing"
+
+	logger "github.com/bxrne/launchrail/internal/logger"
+	"github.com/bxrne/launchrail/internal/plugin"
 
 	"github.com/stretchr/testify/require"
-	"github.com/zerodha/logf"
 )
 
 // compileTestPlugin creates and compiles a simple dummy plugin for testing.
@@ -84,7 +84,7 @@ func main() {} // Required for compilation`, pluginName)
 
 	// Compile the plugin
 	cmd := exec.Command(goExecutable, "build", "-buildmode=plugin", "-o", outputPath, "plugin.go") // Use relative path
-	cmd.Dir = pluginSourceDir // Set working directory
+	cmd.Dir = pluginSourceDir                                                                      // Set working directory
 	output, err := cmd.CombinedOutput()
 	require.NoError(t, err, "Plugin compilation failed: %s", string(output))
 
@@ -127,38 +127,45 @@ func findProjectRoot(startPath string) (string, error) {
 
 // TEST: GIVEN a logger WHEN NewManager is called THEN a new Manager is returned
 func TestNewManager(t *testing.T) {
-	l := logf.Logger{}
-	m := plugin.NewManager(l)
+	l := logger.GetLogger("debug")
+	m := plugin.NewManager(*l)
 	require.NotNil(t, m)
 	require.Empty(t, m.GetPlugins())
 }
 
 // TEST: GIVEN a plugin manager WHEN LoadPlugin is called with an invalid path THEN the plugin manager returns an error
 func TestLoadPluginInvalidPath(t *testing.T) {
-	l := logf.Logger{}
-	m := plugin.NewManager(l)
+	l := logger.GetLogger("debug")
+	m := plugin.NewManager(*l)
 	err := m.LoadPlugin("invalid/path/plugin.so")
 	require.Error(t, err)
 }
 
 // TEST: GIVEN a valid compiled plugin WHEN LoadPlugin is called THEN the plugin is loaded successfully
 func TestLoadPluginSuccess(t *testing.T) {
-	pluginPath, cleanup := compileTestPlugin(t, "testplugin1")
-	defer cleanup()
+	l := logger.GetLogger("debug")
 
-	l := logf.Logger{}
-	m := plugin.NewManager(l)
+	// Compile plugins first and check for errors.
+	// The actual path needed for loading will be constructed later.
+	err := plugin.CompileAllPlugins("../../plugins", "../../plugins", *l)
+	if err != nil {
+		t.Fatalf("CompileAllPlugins failed: %v", err)
+	}
 
-	err := m.LoadPlugin(pluginPath)
+	// INFO: Add any built-in plugins here for confidence
+	m := plugin.NewManager(*l)
+	err = m.LoadPlugin("../../plugins/windeffect.so")
 	require.NoError(t, err)
+	require.NotEmpty(t, m.GetPlugins())
 
-	loadedPlugins := m.GetPlugins()
-	require.Len(t, loadedPlugins, 1, "Expected 1 plugin to be loaded")
+	err = m.LoadPlugin("../../plugins/blackscholes.so")
+	require.NoError(t, err)
+	require.NotEmpty(t, m.GetPlugins())
 }
 
 // TEST: GIVEN a plugin manager WHEN GetPlugins is called THEN the plugin manager returns the loaded plugins
 func TestGetPlugins(t *testing.T) {
-	l := logf.Logger{}
-	m := plugin.NewManager(l)
+	l := logger.GetLogger("debug")
+	m := plugin.NewManager(*l)
 	require.Empty(t, m.GetPlugins())
 }
